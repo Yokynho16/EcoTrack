580adb453914c082649b7b233ae5710f155caa72
Yokynho16 <u202212560@upc.edu.pe>
2024-09-22 17:49:06 -0500

    Initial commit

---
+++ src/test/java/pe/edu/upc/ecotrack/EcoTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.ecotrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
