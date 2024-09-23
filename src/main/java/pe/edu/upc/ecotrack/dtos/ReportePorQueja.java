33403124ba10f64ed3921247edc346a87915cc86
06KJ <144300706+06KJ@users.noreply.github.com>
2024-09-23 04:03:54 +0000

    Creacion Del Query Buscar Reporte por Queja

---
+++ src/main/java/pe/edu/upc/ecotrack/dtos/ReportePorQueja.java
@@ -0,0 +1,22 @@
+package pe.edu.upc.ecotrack.dtos;
+
+public class ReportePorQuejaDTO {
+    private String titulo;
+    private String respuesta;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+}
